443588012a2cb37b19be6e315eecbd11d56479b4
facundolcolavini <fcolavini@southernhorizon.tech>
2024-08-25 16:06:38 -0300

    feat: Add navigation items interface
    chore: Add tailwind.config.mjs
    
    chore: Update astro.config.mjs
    
    chore: Update package.json dependencies
    
    chore: Update tsconfig.json paths
    
    chore: Add SectionContainer component
    
    chore: Add navigation constants
    
    chore: Update index.astro layout
    
    fix: Alias paths of icons

---
+++ src/constants/navigation.ts
@@ -0,0 +1,28 @@
+import type { NavItem } from "@interfaces/nav-items";
+
+export const  navItems:NavItem[] = [
+    {
+        
+        title: 'Experience',
+        label: 'experience',
+        url: '/#experience',
+    },
+    {
+      
+        title: 'Projects',
+        label: 'projects',
+        url: '/#projects',
+    },
+    {
+       
+        title: 'About',
+        label: 'about',
+        url: '/#about',
+    },
+    {
+        
+        title: 'Contact',
+        label: 'contact',
+        url: '/#contact',
+    },
+]
---
+++ src/interfaces/nav-items.ts
@@ -0,0 +1,6 @@
+export interface NavItem {
+    title: string;
+    label:string
+    url: string;
+    icon?: string;
+}
